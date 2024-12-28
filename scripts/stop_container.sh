1af0cb9ab35b8da9914a6eb497a93d558f4d45f4
venkrishneni <venkrishneni@gmail.com>
2024-12-28 12:37:12 -0800

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 echo "Executing stop container"
-# Stop the running container (if any)
-containerId=$(docker ps | aws -F" " '{print $1}')
+# # Stop the running container (if any)
+# containerId=$(docker ps | aws -F" " '{print $1}')
 
-if [ -n "$containerId" ]; then
-  # Stop and remove all containers
-  docker rm -f "$containerId"
-  echo "Container $containerId stopped and removed."
-else
-  echo "No container is running."
-fi
+# if [ -n "$containerId" ]; then
+#   # Stop and remove all containers
+#   docker rm -f "$containerId"
+#   echo "Container $containerId stopped and removed."
+# else
+#   echo "No container is running."
+# fi
