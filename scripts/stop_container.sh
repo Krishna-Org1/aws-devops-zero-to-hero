7bc35b9eab6168a96681946bd58d57d8e7e1da1d
venkrishneni <venkrishneni@gmail.com>
2024-12-28 13:57:17 -0800

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,13 +3,13 @@ set -e
 set -x
 
 echo "Executing stop container"
-# Stop the running container (if any)
-containerId=$(docker ps -q)
+# # Stop the running container (if any)
+# containerId=$(docker ps -q)
 
-if [ -n "$containerId" ]; then
-  # Stop and remove all containers
-  docker rm -f "$containerId"
-  echo "Container $containerId stopped and removed."
-else
-  echo "No container is running."
-fi
+# if [ -n "$containerId" ]; then
+#   # Stop and remove all containers
+#   docker rm -f "$containerId"
+#   echo "Container $containerId stopped and removed."
+# else
+#   echo "No container is running."
+# fi
